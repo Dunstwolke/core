d6130a1aa4f44e9cb2c5baa8047a9e6bd4404ec1
Felix (xq) Queißner <git@mq32.de>
2020-07-16 01:26:05 +0200

    I fucked up so hard. Trying to fix.

--- build.zig
+++ build.zig
@@ -4,22 +4,22 @@ const Builder = std.build.Builder;
 const pkgs = struct {
     const network = std.build.Pkg{
         .name = "network",
-        .path = "./ext/zig-network/network.zig",
+        .path = "./lib/zig-network/network.zig",
     };
 
     const sdl2 = std.build.Pkg{
         .name = "sdl2",
-        .path = "./ext/zig-sdl/src/lib.zig",
+        .path = "./lib/zig-sdl/src/lib.zig",
     };
 
     const args = std.build.Pkg{
         .name = "args",
-        .path = "./ext/zig-args/args.zig",
+        .path = "./lib/zig-args/args.zig",
     };
 
     const uri = std.build.Pkg{
         .name = "uri",
-        .path = "./ext/zig-uri/uri.zig",
+        .path = "./lib/zig-uri/uri.zig",
     };
 };
 
@@ -27,16 +27,16 @@ pub fn build(b: *Builder) !void {
     const mode = b.standardReleaseOptions();
     const target = b.standardTargetOptions(.{});
 
-    const compiler = b.addExecutable("dunstblick-compiler", "./dunstblick-compiler/main.zig");
+    const compiler = b.addExecutable("dunstblick-compiler", "./src/dunstblick-compiler/main.zig");
     compiler.addPackagePath("args", "./ext/zig-args/args.zig");
-    compiler.addIncludeDir("./libdunstblick/include");
+    compiler.addIncludeDir("./src/libdunstblick/include");
     compiler.setTarget(target);
     compiler.setBuildMode(mode);
     compiler.install();
 
-    const compiler_test = b.addTest("./dunstblick-compiler/main.zig");
+    const compiler_test = b.addTest("./src/dunstblick-compiler/main.zig");
 
-    const lib = b.addStaticLibrary("dunstblick", "./libdunstblick/src/dunstblick.zig");
+    const lib = b.addStaticLibrary("dunstblick", "./src/libdunstblick/src/dunstblick.zig");
     lib.addPackage(pkgs.network);
     lib.addIncludeDir("./libdunstblick/include");
     lib.linkLibC();
@@ -45,19 +45,19 @@ pub fn build(b: *Builder) !void {
     lib.install();
 
     // mediaserver project
-    const mediaserver = b.addExecutable("mediaserver", "./examples/mediaserver/src/main.zig");
-    mediaserver.addIncludeDir("./libdunstblick/include");
-    mediaserver.addIncludeDir("./examples/mediaserver/bass");
-    mediaserver.addLibPath("./examples/mediaserver/bass/x86_64");
+    const mediaserver = b.addExecutable("mediaserver", "./src/examples/mediaserver/src/main.zig");
+    mediaserver.addIncludeDir("./src/libdunstblick/include");
+    mediaserver.addIncludeDir("./src/examples/mediaserver/bass");
+    mediaserver.addLibPath("./src/examples/mediaserver/bass/x86_64");
     mediaserver.linkSystemLibrary("bass");
     mediaserver.linkLibrary(lib);
     mediaserver.install();
 
     const layout_files = [_][]const u8{
-        "./examples/mediaserver/layouts/main.dui",
-        "./examples/mediaserver/layouts/menu.dui",
-        "./examples/mediaserver/layouts/searchlist.dui",
-        "./examples/mediaserver/layouts/searchitem.dui",
+        "./src/examples/mediaserver/layouts/main.dui",
+        "./src/examples/mediaserver/layouts/menu.dui",
+        "./src/examples/mediaserver/layouts/searchlist.dui",
+        "./src/examples/mediaserver/layouts/searchitem.dui",
     };
     inline for (layout_files) |infile| {
         const outfile = try std.mem.dupe(b.allocator, u8, infile);
@@ -69,7 +69,7 @@ pub fn build(b: *Builder) !void {
             "-o",
             outfile,
             "-c",
-            "./examples/mediaserver/layouts/server.json",
+            "./src/examples/mediaserver/layouts/server.json",
         });
         mediaserver.step.dependOn(&step.step);
     }
@@ -81,8 +81,8 @@ pub fn build(b: *Builder) !void {
 
     // calculator example
     const calculator = b.addExecutable("calculator", null);
-    calculator.addIncludeDir("./libdunstblick/include");
-    calculator.addCSourceFile("examples/calculator/main.c", &[_][]const u8{});
+    calculator.addIncludeDir("./src/libdunstblick/include");
+    calculator.addCSourceFile("src/examples/calculator/main.c", &[_][]const u8{});
     calculator.linkLibrary(lib);
     calculator.setTarget(target);
     calculator.setBuildMode(mode);
@@ -90,26 +90,26 @@ pub fn build(b: *Builder) !void {
 
     const calculator_headerGen = compiler.run();
     calculator_headerGen.addArgs(&[_][]const u8{
-        "./examples/calculator/layout.ui",
+        "./src/examples/calculator/layout.ui",
         "-o",
-        "./examples/calculator/layout.h",
+        "./src/examples/calculator/layout.h",
         "-f",
         "header",
         "-c",
-        "./examples/calculator/layout.json",
+        "./src/examples/calculator/layout.json",
     });
     calculator.step.dependOn(&calculator_headerGen.step);
 
     // minimal example
     const minimal = b.addExecutable("minimal", null);
-    minimal.addIncludeDir("./libdunstblick/include");
-    minimal.addCSourceFile("examples/minimal/main.c", &[_][]const u8{});
+    minimal.addIncludeDir("./src/libdunstblick/include");
+    minimal.addCSourceFile("src/examples/minimal/main.c", &[_][]const u8{});
     minimal.linkLibrary(lib);
     minimal.setTarget(target);
     minimal.setBuildMode(mode);
     minimal.install();
 
-    const display_client = b.addExecutable("dunstblick-display", "./dunstblick-display/main.zig");
+    const display_client = b.addExecutable("dunstblick-display", "./src/dunstblick-display/main.zig");
 
     display_client.addPackage(pkgs.network);
     display_client.addPackage(pkgs.args);
@@ -119,17 +119,17 @@ pub fn build(b: *Builder) !void {
     display_client.linkLibC();
     display_client.linkSystemLibrary("c++");
     display_client.linkSystemLibrary("sdl2");
-    display_client.addIncludeDir("./libdunstblick/include");
-    display_client.addIncludeDir("./ext/xqlib/include");
-    display_client.addIncludeDir("./ext/xqlib/extern/optional/tl");
-    display_client.addIncludeDir("./ext/xqlib/extern/GSL/include");
-    display_client.addIncludeDir("./ext/stb");
-    display_client.defineCMacro("DUNSTBLICK_SERVER");
+    // display_client.addIncludeDir("./src/libdunstblick/include");
+    // display_client.addIncludeDir("./lib/xqlib/include");
+    // display_client.addIncludeDir("./lib/xqlib/extern/optional/tl");
+    // display_client.addIncludeDir("./lib/xqlib/extern/GSL/include");
+    // display_client.addIncludeDir("./lib/stb");
+    // display_client.defineCMacro("DUNSTBLICK_SERVER");
     display_client.setBuildMode(mode);
     display_client.setTarget(target);
     display_client.install();
 
-    display_client.addCSourceFile("./dunstblick-display/stb-instantiating.c", &[_][]const u8{
+    display_client.addCSourceFile("./src/dunstblick-display/stb-instantiating.c", &[_][]const u8{
         "-std=c99",
     });
 
@@ -150,7 +150,7 @@ pub fn build(b: *Builder) !void {
     const run_cmd = display_client.run();
     run_cmd.step.dependOn(b.getInstallStep());
 
-    run_cmd.setEnvironmentVariable("LD_LIBRARY_PATH", "examples/mediaserver/bass/x86_64");
+    run_cmd.setEnvironmentVariable("LD_LIBRARY_PATH", "./src/examples/mediaserver/bass/x86_64");
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
@@ -160,32 +160,32 @@ pub fn build(b: *Builder) !void {
 }
 
 const display_client_sources = [_][]const u8{
-    "./dunstblick-display/api.cpp",
-    "./dunstblick-display/enums.cpp",
-    "./dunstblick-display/fontcache.cpp",
-    "./dunstblick-display/inputstream.cpp",
-    "./dunstblick-display/layouts.cpp",
-    "./dunstblick-display/localsession.cpp",
-    "./dunstblick-display/main.cpp",
-    "./dunstblick-display/networksession.cpp",
-    "./dunstblick-display/object.cpp",
-    "./dunstblick-display/protocol.cpp",
-    "./dunstblick-display/rendercontext.cpp",
-    "./dunstblick-display/resources.cpp",
-    "./dunstblick-display/session.cpp",
-    "./dunstblick-display/tcphost.cpp",
-    "./dunstblick-display/testhost.cpp",
-    "./dunstblick-display/types.cpp",
-    "./dunstblick-display/widget.cpp",
-    "./dunstblick-display/widget.create.cpp",
-    "./dunstblick-display/widgets.cpp",
+    "./src/dunstblick-display/api.cpp",
+    "./src/dunstblick-display/enums.cpp",
+    "./src/dunstblick-display/fontcache.cpp",
+    "./src/dunstblick-display/inputstream.cpp",
+    "./src/dunstblick-display/layouts.cpp",
+    "./src/dunstblick-display/localsession.cpp",
+    "./src/dunstblick-display/main.cpp",
+    "./src/dunstblick-display/networksession.cpp",
+    "./src/dunstblick-display/object.cpp",
+    "./src/dunstblick-display/protocol.cpp",
+    "./src/dunstblick-display/rendercontext.cpp",
+    "./src/dunstblick-display/resources.cpp",
+    "./src/dunstblick-display/session.cpp",
+    "./src/dunstblick-display/tcphost.cpp",
+    "./src/dunstblick-display/testhost.cpp",
+    "./src/dunstblick-display/types.cpp",
+    "./src/dunstblick-display/widget.cpp",
+    "./src/dunstblick-display/widget.create.cpp",
+    "./src/dunstblick-display/widgets.cpp",
 };
 
 const xqlib_sources = [_][]const u8{
-    "./ext/xqlib/src/sdl2++.cpp",
-    "./ext/xqlib/src/xio.cpp",
-    "./ext/xqlib/src/xlog.cpp",
-    "./ext/xqlib/src/xnet.cpp",
-    "./ext/xqlib/src/xception.cpp",
-    "./ext/xqlib/src/xstd_format.cpp",
+    "./lib/xqlib/src/sdl2++.cpp",
+    "./lib/xqlib/src/xio.cpp",
+    "./lib/xqlib/src/xlog.cpp",
+    "./lib/xqlib/src/xnet.cpp",
+    "./lib/xqlib/src/xception.cpp",
+    "./lib/xqlib/src/xstd_format.cpp",
 };
